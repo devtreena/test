9e92fa563efedde151462dbfe2ba763150747a49
Tree <devtreena@gmail.com>
2022-01-22 15:01:57 +0700

    kkkk

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+const path = require('path');
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
